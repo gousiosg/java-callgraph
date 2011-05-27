9c5bd1d5f4fa63fdfcbaed9a709425e2f85bc964
Georgios Gousios <gousiosg@gmail.com>
2011-05-27 09:24:20 +0300

    Simple maven project

---
+++ src/main/java/gr/gousiosg/callgraph/App.java
@@ -0,0 +1,13 @@
+package gr.gousiosg.callgraph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
