b85c1545ffe93346161e1a37e4ebc1ba8469a84f
Georgios Gousios <gousiosg@gmail.com>
2018-10-24 09:44:18 +0200

    Change license as per author's request
    fixes: #23

--- src/main/java/gr/gousiosg/javacg/stat/DynamicCallManager.java
+++ src/main/java/gr/gousiosg/javacg/stat/DynamicCallManager.java
@@ -1,29 +1,14 @@
 /*
- * Copyright (c) 2018 - Matthieu Vergne <matthieu.vergne@gmail.com>
+ * Written in 2018 by Matthieu Vergne <matthieu.vergne@gmail.com>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
+ *     To the extent possible under law, the author(s) have dedicated all
+ *     copyright and related and neighboring rights to this software to
+ *     the public domain worldwide. This software is distributed without
+ *     any warranty.
  *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *     You should have received a copy of the CC0 Public Domain Dedication
+ *     along with this software. If not,
+ *     see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 
 package gr.gousiosg.javacg.stat;
